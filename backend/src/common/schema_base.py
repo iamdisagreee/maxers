8aed1906b9fc8dc57790bb0cb5f3de314a064267
vova <vavakharitonov2004@gmail.com>
2025-11-15 09:53:47 +0300

    New merge

--- backend/src/common/schema_base.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, ConfigDict, alias_generators, Field, EmailStr
-
-
-class CamelCaseModel(BaseModel):
-    model_config = ConfigDict(
-        validate_by_name=True,
-        alias_generator=alias_generators.to_camel,
-        from_attributes=True,
-    )
